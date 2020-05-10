5fbca3e87151c96de9fe3de28afb2f2567e6086c
maxim <maximopolnate@gmail.com>
2020-05-10 20:47:10 +0300

    Initial commit

---
+++ src/main/java/com/maximopol/driveopol/DriveopolApplication.java
@@ -0,0 +1,13 @@
+package com.maximopol.driveopol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveopolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveopolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maximopol/driveopol/DriveopolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximopol.driveopol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveopolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
